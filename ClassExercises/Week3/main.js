1a6360077999febfac24bc234a260548c1e90ced
Robert Blazeski <robert.blazeski5939@gmail.com>
2025-10-07 16:40:45 +0200

    Week 3 Assignment

--- ClassExercises/Week3/main.js
+++ ClassExercises/Week3/main.js
@@ -1,70 +1,65 @@
 import * as THREE from 'three';
 
+
+
+// Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x202020);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 10;
-camera.position.y=3
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000
+);
+camera.position.set(1, 6, 6);
+camera.rotation.x=-0.6
 
-const renderer = new THREE.WebGLRenderer({antialias:true});
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// light
-const light = new THREE.DirectionalLight(0xffffff,1);
-light.position.set(5,5,5);
-scene.add(light);
+// plane floor
+const floorGeometry = new THREE.PlaneGeometry(10, 10);
+const floorMaterial = new THREE.MeshStandardMaterial({ color: 0x333333 });
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -Math.PI / 2;
+scene.add(floor);
 
-// house group
-const house = new THREE.Group(); 
+// Shapes
 
-// walls
-const wallGeo = new THREE.BoxGeometry(3,2,3);
-const wallMat = new THREE.MeshStandardMaterial({color:0xffaa00});
-const walls = new THREE.Mesh(wallGeo, wallMat);
-walls.position.y = 1; 
-house.add(walls); 
+const boxGeometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xff4444 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(-1, 0.5, 0);
+scene.add(box);
 
-// roof
-const roofGeo = new THREE.ConeGeometry(2, 1.5, 3);
-const roofMat = new THREE.MeshStandardMaterial({color:0xff0000});
-const roof = new THREE.Mesh(roofGeo, roofMat);
-roof.position.y = 2.65; 
-roof.position.z=1
- 
-roof.position.x=-0.2
-roof.rotation.y = Math.PI / 2.4;
-house.add(roof); 
+const sphereGeometry = new THREE.SphereGeometry(0.7, 12, 32);
+const sphereMaterial = new THREE.MeshPhongMaterial({ color: 0x44ff44 });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(0.8, 0.75, 2);
+scene.add(sphere);
 
-// windows
-const windowGeo = new THREE.CircleGeometry(0.3, 32);
-const windowMat = new THREE.MeshStandardMaterial({color:0x00ffff, metalness:0.7, roughness:0.1});
-const window1 = new THREE.Mesh(windowGeo, windowMat);
-window1.position.set(-0.8, 1.5, 1.51);
-house.add(window1);
+const coneGeometry = new THREE.ConeGeometry(1, 2, 12);
+const coneMaterial = new THREE.MeshLambertMaterial({ color: 0x4488ff });
+const cone = new THREE.Mesh(coneGeometry, coneMaterial);
+cone.position.set(2, 0.75, 0);
 
-const window2 = new THREE.Mesh(windowGeo, windowMat);
-window2.position.set(0.8, 1.5, 1.51);
-house.add(window2); 
+scene.add(cone);
 
-// add hosue to scene
-scene.add(house);
+// LIghts
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambientLight);
 
-// Axes hlper
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(3, 5, 2);
+scene.add(directionalLight);
 
 // Animation
-function animate(){
-    requestAnimationFrame(animate);
+function animate() {
+  requestAnimationFrame(animate);
 
-   
-    // house.rotation.y += 0.001;
-    
+  box.rotation.y += 0.01;
+  sphere.rotation.y += 0.01;
+  cone.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
-
 animate();
